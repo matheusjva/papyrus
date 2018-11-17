b8ec483bde4d9942a4913f3d86a85b5d78ecb7ba
Matheus Albino <matheusjva@gmail.com>
2018-11-16 22:24:30 -0200

    Migration da tabela de TCCS

---
+++ database/migrations/2018_11_17_001918_create_works_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('creator_id')->unsigned();
+            $table->string('title', 255);
+            $table->string('description', 255);
+            $table->string('authors', 255);
+            $table->string('year', 4);
+            $table->string('jury', 255);
+            $table->string('path_file', 100);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('creator_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
