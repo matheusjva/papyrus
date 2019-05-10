e2fe3f86cfc56089a86d8e780e19a0d697051d39
Matheus Albino <matheusjva@gmail.com>
2019-05-10 13:03:04 -0300

    wip: layout login.

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::get('teste', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/login2', function(){
+    return view('auth.login2');
+});
